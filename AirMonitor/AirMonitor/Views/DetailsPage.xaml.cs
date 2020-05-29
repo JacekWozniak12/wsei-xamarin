8babfed7dc8a76833ec6a7ddad761662b67e7c29
jacekwozniak12 <jac3kw0zniak@gmail.com>
2020-05-29 12:02:52 +0200

    Zadanie 2.1

--- AirMonitor/AirMonitor/Views/DetailsPage.xaml.cs
+++ AirMonitor/AirMonitor/Views/DetailsPage.xaml.cs
@@ -11,16 +11,16 @@ namespace AirMonitor.Views
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class MainPage : ContentPage
+    public partial class DetailsPage : ContentPage
     {
-        public MainPage()
+        public DetailsPage()
         {
             InitializeComponent();
         }
 
         private void Help_Clicked(object sender, EventArgs e)
         {
-            DisplayAlert("Co to jest?", "Lorem ipsum.", "Zamknij");
+            DisplayAlert("Co to jest tex?", "Lorem ipsum.", "Zamknij");
         }
     }
 }
