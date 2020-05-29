dffcc2e5c0a7b56ca0ca57ed3f3490d1c0e6e380
jacekwozniak12 <jac3kw0zniak@gmail.com>
2020-05-29 21:39:20 +0200

    Zadanie 3

--- AirMonitor/AirMonitor/App.xaml.cs
+++ AirMonitor/AirMonitor/App.xaml.cs
@@ -1,17 +1,33 @@
 ﻿using System;
 using AirMonitor.Views;
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 using Xamarin.Forms.Xaml;
+using TabbedPage = Xamarin.Forms.TabbedPage;
 
 namespace AirMonitor
 {
-    public partial class App : Application
+    public partial class App : Xamarin.Forms.Application
     {
         public App()
         {
             InitializeComponent();
+            MainPage = CreateTabbedPage();
+        }
 
-            MainPage = new NavigationPage(new HomePage());
+        private TabbedPage CreateTabbedPage()
+        {
+            var TP = new TabbedPage();
+            TP.Title = "Tabbed Page";
+            TP.Children.Add(new SettingsPage{ 
+                Title = "Settings" 
+            });
+            TP.Children.Add(new HomePage { 
+                Title = "Home" 
+            });
+            TP.On<Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
+            return TP;
         }
 
         protected override void OnStart()
